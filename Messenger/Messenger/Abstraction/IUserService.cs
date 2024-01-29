ac8fc615902a619e7605cfe393731fa876f452a7
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2024-01-29 21:57:49 +0700

    123

--- Messenger/Messenger/Abstraction/IUserService.cs
+++ Messenger/Messenger/Abstraction/IUserService.cs
@@ -4,7 +4,7 @@ namespace SEM4_Swagger.Abstraction
 {
     public interface IUserService
     {
-        public Guid UserAdd(string name, string password, UserRole roleId);
+        public Guid UserAdd(string name, string password);
         public string UserCheckRole(string name, string password);
     }
 }
