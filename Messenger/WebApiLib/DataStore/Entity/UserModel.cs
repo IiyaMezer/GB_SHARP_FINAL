036b43c331d329dab91b70ebd59a3344341bbfb8
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2024-01-30 19:18:03 +0700

    Account class

--- Messenger/WebApiLib/DataStore/Entity/UserModel.cs
+++ Messenger/WebApiLib/DataStore/Entity/UserModel.cs
@@ -7,8 +7,9 @@ namespace WebApiLib.DataStore.Entity
 {
     public class UserModel
     {
+        public Guid? Id { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
-        public UserRole Role { get; set; }
+        public UserRole? Role { get; set; }
     }
 }
