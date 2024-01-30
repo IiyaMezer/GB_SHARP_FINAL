a2dd5e8f10486d0462ea8c79b3c6ffaea3a922a3
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2024-01-30 22:38:58 +0700

    регистрация компонентов

--- Messenger/UserApi/Program.cs
+++ Messenger/UserApi/Program.cs
@@ -1,4 +1,15 @@
 
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using UserApi.Services;
+using WebApiLib;
+using WebApiLib.Abstraction;
+using WebApiLib.DataStore.Entity;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using WebApiLib.Rsa;
+
 namespace UserApi
 {
     public class Program
@@ -10,9 +21,59 @@ namespace UserApi
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddSingleton<Account>();
+            builder.Services.AddSingleton<IUserService, UserService>();
+            builder.Services.AddAutoMapper(typeof(UserProfile));
+            builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+            builder.Host.ConfigureContainer<ContainerBuilder>(c =>
+            {
+                c.Register(c => new AppDbContext(builder.Configuration
+                    .GetConnectionString("DefaultConnection")))
+                    .InstancePerDependency();
+            });
+
+            builder.Services.AddSwaggerGen(x =>
+            {
+                x.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Enter token",
+                    Name = "Authorisation",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "Token",
+                    Scheme = "bearer"
+                });
+                x.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
+            });
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+            opt.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                ValidAudience = builder.Configuration["Jwt:Audience"],
+                IssuerSigningKey = new RsaSecurityKey(RsaService.GetPublicKey())
+            });
+
+
 
             var app = builder.Build();
 
